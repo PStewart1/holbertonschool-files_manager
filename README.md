ea68b2d8de03c8adafe30e56cb3c8c9296b2664b
PStewart1 <4920@holbertonstudents.com>
2023-11-04 00:53:24 -0500

    Redis utils

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# holbertonschool-files_manager
+# Files manager
 This project is a summary of this back-end trimester: authentication, NodeJS, MongoDB, Redis, pagination and background processing.
+
+### Learning Objectives:
+*    how to create an API with Express
+*    how to authenticate a user
+*    how to store data in MongoDB
+*    how to store temporary data in Redis
+*    how to setup and use a background worker
+
+--- 
+## Author 
+#### Paul S.
---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+import * as redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const value = await this.client.get(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
