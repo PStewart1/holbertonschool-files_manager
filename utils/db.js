0bd65c6a6880d3514673b62940119e7ac3df9587
PStewart1 <4920@holbertonstudents.com>
2023-11-19 13:57:40 -0600

    linted

--- utils/db.js
+++ utils/db.js
@@ -1,13 +1,13 @@
-const { MongoClient } = require("mongodb");
-let host = process.env.DB_HOST
-let port = process.env.DB_PORT
-let database = process.env.DB_DATABASE
+const { MongoClient } = require('mongodb');
+
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const database = process.env.DB_DATABASE;
 
 class DBClient {
   constructor(host = 'localhost', port = 27017, database = 'files_manager') {
-    // this.database = database;
     const url = `mongodb://${host}:${port}/${database}`;
-    this.client = new MongoClient(url)
+    this.client = new MongoClient(url);
     this.client.connect();
   }
 
@@ -17,18 +17,18 @@ class DBClient {
 
   async nbUsers() {
     const db = this.client.db();
-    const users = db.collection("users");
+    const users = db.collection('users');
     const numUsers = await users.countDocuments();
     return numUsers;
   }
 
   async nbFiles() {
     const db = this.client.db();
-    const files = db.collection("files");
+    const files = db.collection('files');
     const numFiles = await files.countDocuments();
     return numFiles;
   }
 }
 
 const dbClient = new DBClient();
-export default dbClient
+export default dbClient;
