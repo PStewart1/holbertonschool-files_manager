cd314e1a8ce4b11ae49647260dc1dd647fde98af
PStewart1 <4920@holbertonstudents.com>
2023-11-19 13:50:42 -0600

    first attempt

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,34 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require("mongodb");
+let host = process.env.DB_HOST
+let port = process.env.DB_PORT
+let database = process.env.DB_DATABASE
 
 class DBClient {
-  constructor() {
+  constructor(host = 'localhost', port = 27017, database = 'files_manager') {
+    // this.database = database;
+    const url = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(url)
+    this.client.connect();
+  }
 
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const db = this.client.db();
+    const users = db.collection("users");
+    const numUsers = await users.countDocuments();
+    return numUsers;
+  }
+
+  async nbFiles() {
+    const db = this.client.db();
+    const files = db.collection("files");
+    const numFiles = await files.countDocuments();
+    return numFiles;
   }
 }
+
+const dbClient = new DBClient();
+export default dbClient
