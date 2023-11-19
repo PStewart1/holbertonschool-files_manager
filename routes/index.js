13ca2c289c5cc7ccfe266b377cdbc04e4b31b252
Douglas <wdmdavison@icloud.com>
2023-11-19 21:46:16 +0000

    add routes to index for 3 and 4

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 // contains all the endpoints of our API
 
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UserController';
+import AuthController from '../controllers/AuthController';
 
 const express = require('express');
 
@@ -8,5 +10,10 @@ const routesToUse = express.Router();
 
 routesToUse.get('/status', AppController.getStatus);
 routesToUse.get('/stats', AppController.getStats);
+routesToUse.post('/users', UsersController.postNew);
+routesToUse.get('/connect', AuthController.getConnect);
+routesToUse.get('/disconnect', AuthController.getDisconnect);
+routesToUse.get('/users/me', UsersController.getMe);
+
 
 module.exports = routesToUse;
