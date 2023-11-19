34c4cf8d8b8b682879a5b07fd05b25bba0ee5c72
PStewart1 <4920@holbertonstudents.com>
2023-11-19 17:31:24 -0600

    Update routes and import UsersController

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 // contains all the endpoints of our API
 
-import AppController from '../controllers/AppController'
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UserController';
+// import AuthController from '../controllers/AuthController';
+
 const express = require('express');
 
 const routesToUse = express.Router();
 
 routesToUse.get('/status', AppController.getStatus);
-// routesToUse.get('/stats', AppController.getStats);
+routesToUse.get('/stats', AppController.getStats);
+routesToUse.post('/users', UsersController.postNew);
+// routesToUse.get('/connect', AuthController.getConnect);
+// routesToUse.get('/disconnect', AuthController.getDisconnect);
+// routesToUse.get('/users/me', UsersController.getMe);
+
 
 module.exports = routesToUse;
