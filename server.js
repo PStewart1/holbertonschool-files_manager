68ff76b49ad4cf6e30b8c5930a90cb806f8ae975
PStewart1 <4920@holbertonstudents.com>
2023-11-19 14:32:09 -0600

    basic server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+let port = 5000;
+if (process.env.PORT) {
+  port = process.env.PORT;
+}
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
